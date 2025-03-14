69a938b01b8c40299fc9a022eb14126fa13c1416
Evan Xiong <evanxiong8@gmail.com>
2025-03-14 00:58:23 -0400

    Add OAuth2 flow to server

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,11 @@ app.use(express.json());
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 
+app.get("/", (req, res) => {
+  console.log("Received GET request");
+  res.send("notion-movies server is working");
+});
+
 app.post("/", async (req, res) => {
   // Process POST requests from webhook
   console.log("Received POST request");
@@ -48,6 +53,57 @@ app.post("/", async (req, res) => {
     }
 
     res.send("Completed POST request.");
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.get("/auth", async (req, res) => {
+  console.log("Received auth request");
+  try {
+    if (req.query.error) {
+      // Handle canceled request or error
+      console.log(req.query.error);
+      res.redirect("https://github.com/evxiong/notion-movies");
+      return;
+    }
+    if (req.query.code) {
+      // Send POST request for access token, using temp auth code
+      console.log(req.query.code);
+
+      const encoded = Buffer.from(
+        `${process.env.OAUTH_CLIENT_ID}:${process.env.OAUTH_CLIENT_SECRET}`
+      ).toString("base64");
+
+      const options = {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          Authorization: `Basic ${encoded}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          grant_type: "authorization_code",
+          code: req.query.code,
+          redirect_uri: process.env.OAUTH_REDIRECT_URI,
+        }),
+      };
+
+      const r = await fetch("https://api.notion.com/v1/oauth/token", options);
+      const response = await r.json();
+      console.log(response);
+
+      if (response.error) {
+        // Handle token request failure
+        res.redirect("https://github.com/evxiong/notion-movies");
+        return;
+      }
+      // Write user info to MongoDB
+      const users = client.db("notion").collection("users");
+      const result = await users.insertOne(response); // should change to upsert?
+      console.log(`Inserted new document with _id: ${result.insertedId}`);
+      res.send("Auth flow complete");
+    }
   } catch (e) {
     console.error(e);
   }
