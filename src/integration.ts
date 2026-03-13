e7b9f4d1c9fc8db496761b5ceb7b9ae647a7aaf3
Evan Xiong <evanxiong8@gmail.com>
2026-03-13 15:45:25 -0400

    Fix Vercel error
    See https://github.com/vercel/pkg/issues/910

--- src/integration.ts
+++ src/integration.ts
@@ -1,6 +1,28 @@
 import { Client } from "@notionhq/client";
 import type { Browser } from "puppeteer-core";
 import { addExtra } from "puppeteer-extra";
+
+// See https://github.com/vercel/pkg/issues/910
+import "puppeteer-extra-plugin-stealth/evasions/chrome.app";
+import "puppeteer-extra-plugin-stealth/evasions/chrome.csi";
+import "puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes";
+import "puppeteer-extra-plugin-stealth/evasions/chrome.runtime";
+import "puppeteer-extra-plugin-stealth/evasions/defaultArgs";
+import "puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow";
+import "puppeteer-extra-plugin-stealth/evasions/media.codecs";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.languages";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.permissions";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.plugins";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.vendor";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.webdriver";
+import "puppeteer-extra-plugin-stealth/evasions/sourceurl";
+import "puppeteer-extra-plugin-stealth/evasions/user-agent-override";
+import "puppeteer-extra-plugin-stealth/evasions/webgl.vendor";
+import "puppeteer-extra-plugin-stealth/evasions/window.outerdimensions";
+import "puppeteer-extra-plugin-user-data-dir";
+import "puppeteer-extra-plugin-user-preferences";
+
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import {
   getNotionDatabaseId,
